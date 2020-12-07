a02fb8c0c0b3db65ff0a2a352f00b1a6bc585c52
Rob Schwartz <robert.schwartz@target.com>
2020-12-07 14:04:14 -0700

    My first fragment!

--- CriminalIntent/app/src/main/java/com/example/criminalintent/Crime.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/Crime.kt
@@ -2,6 +2,8 @@ package com.example.criminalintent
 
 import java.util.*
 
-data class Crime(var title: String = "", val id: UUID = UUID.randomUUID(), var date: Date = Date(), var isSolved: Boolean = false){
-
+data class Crime(val id: UUID = UUID.randomUUID(),
+                 var title: String = "",
+                 var date: Date = Date(),
+                 var isSolved: Boolean = false) {
 }
--- CriminalIntent/app/src/main/java/com/example/criminalintent/MainActivity.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.criminalintent
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +8,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+        if (currentFragment == null){
+            val fragment = CrimeFragment()
+            supportFragmentManager.beginTransaction().add(R.id.fragment_container, fragment).commit()
+        }
     }
 }
